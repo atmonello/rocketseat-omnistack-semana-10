ed56a39a440c5b9be4a0bc325ad567f876e5e7e9
André Monello <atmonello@gmail.com>
2020-01-18 00:51:41 -0300

    Implementando busca de devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,33 +1,49 @@
 /* eslint-disable no-undef */
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringToArray = require('../utils/parseStringToArray');
 
 module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    if (devs) {
+      return res.json(devs);
+    }
+
+    return res.status(204).json({ message: 'No devs found!' });
+  },
+
   async store(req, res) {
     const {
       github_username, techs, latitude, longitude,
     } = req.body;
 
-    const response = await axios.get(`https://api.github.com/users/${github_username}`);
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = response.data;
+    if (!dev) {
+      const response = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    const techsArray = techs.split(',').map((tech) => tech.trim());
+      const { name = login, avatar_url, bio } = response.data;
 
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    };
+      const techsArray = parseStringToArray(techs);
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location,
-    });
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
 
-    return res.json(dev);
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      });
+
+      return res.status(201).json(dev);
+    }
+    return res.status(400).json({ message: `User ${github_username} already exists` });
   },
 };
