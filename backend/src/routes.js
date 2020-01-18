e9b287209f8b36be2715290b0d6b83189f250120
André Monello <atmonello@gmail.com>
2020-01-18 00:18:29 -0300

    Implementando criação de devs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello OmniStack' }));
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
