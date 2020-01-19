d5c562caba512197a44a70c98611fbd4e6d25614
André Monello <atmonello@gmail.com>
2020-01-18 22:43:48 -0300

    Componentizando formulário e itens de devs

--- src/App.js
+++ src/App.js
@@ -6,147 +6,38 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
-
-    const response = await api.post('/devs', {
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-
-    setGithubUsername('');
-    setTechs('');
-
-    console.log(response.data);
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input
-              id="github_username"
-              name="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              id="techs"
-              name="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                id="latitude"
-                name="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                id="longitude"
-                name="longitude"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/10872514?s=460&v=4" alt="André Monello"/>
-              <div className="user-info">
-                <strong>André Monello</strong>
-                <span>ReactJS, VueJS, Node.js</span>
-              </div>
-            </header>
-            <p>Front-end developer @r7com. Front-end, tech and startups. São Paulo, Brasil.</p>
-            <a href="https://github.com/atmonello">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/10872514?s=460&v=4" alt="André Monello"/>
-              <div className="user-info">
-                <strong>André Monello</strong>
-                <span>ReactJS, VueJS, Node.js</span>
-              </div>
-            </header>
-            <p>Front-end developer @r7com. Front-end, tech and startups. São Paulo, Brasil.</p>
-            <a href="https://github.com/atmonello">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/10872514?s=460&v=4" alt="André Monello"/>
-              <div className="user-info">
-                <strong>André Monello</strong>
-                <span>ReactJS, VueJS, Node.js</span>
-              </div>
-            </header>
-            <p>Front-end developer @r7com. Front-end, tech and startups. São Paulo, Brasil.</p>
-            <a href="https://github.com/atmonello">Acessar perfil no GitHub</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/10872514?s=460&v=4" alt="André Monello"/>
-              <div className="user-info">
-                <strong>André Monello</strong>
-                <span>ReactJS, VueJS, Node.js</span>
-              </div>
-            </header>
-            <p>Front-end developer @r7com. Front-end, tech and startups. São Paulo, Brasil.</p>
-            <a href="https://github.com/atmonello">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
         </ul>
       </main>
     </div>
