767cd67b5d6c1e5b5087fddd2219a1880fa265b0
André Monello <atmonello@gmail.com>
2020-01-20 20:34:23 -0300

    Adicionando variáveis de ambiente

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env'
+});
 
 const routes = require('./routes');
 
+const port = process.env.APP_PORT || 3333;
+const name = process.env.APP_NAME || 'atmonello-omnistack10';
+
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-uggky.mongodb.net/week-10?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-uggky.mongodb.net/week-10?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -15,4 +21,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
+
+console.log(`${name} is running on port ${port}`);
