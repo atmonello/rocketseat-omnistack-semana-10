78f997cf38a4bd26efa241ea6b875f4caf539e7c
André Monello <atmonello@gmail.com>
2020-01-20 21:01:15 -0300

    Adicionando .env exemplo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isStaging = process.env.NODE_ENV === 'staging';
+const isProduction = process.env.NODE_ENV === 'production'
+
+const loadEnv = () => {
+  if (isDev) {
+    return '.env.development';
+  } else if (isStaging) {
+    return '.env.staging';
+  } else if (isProduction) {
+    return '.env.production';
+  }
+  return '.env.example';
+}
+
 require('dotenv').config({
-  path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env'
+  path: loadEnv()
 });
 
 const routes = require('./routes');
@@ -10,9 +26,11 @@ const routes = require('./routes');
 const port = process.env.APP_PORT || 3333;
 const name = process.env.APP_NAME || 'atmonello-omnistack10';
 
+const databaseURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-uggky.mongodb.net/week-10?retryWrites=true&w=majority`, {
+mongoose.connect(databaseURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
