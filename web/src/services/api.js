d007d87ead0d017b987fbc4af79c67d8afd56e15
André Monello <atmonello@gmail.com>
2020-01-20 23:02:33 -0300

    Configurando variavel de ambiente para web

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+console.log('base url', baseURL);
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL
 });
 
 export default api;
