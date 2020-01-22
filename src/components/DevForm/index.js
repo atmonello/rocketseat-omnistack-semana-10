50fd0c8b40926c85e280b03f81297e29ff86f125
André Monello <atmonello@gmail.com>
2020-01-22 18:43:15 -0300

    Removendo geolocalização da web

--- src/components/DevForm/index.js
+++ src/components/DevForm/index.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 export default function DevForm({ onSubmit }) {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
   const [github_username, setGithubUsername] = useState('');
   const [techs, setTechs] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
-  }, []);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -30,8 +12,6 @@ export default function DevForm({ onSubmit }) {
     await onSubmit({
       github_username,
       techs,
-      latitude,
-      longitude
     });
 
     setGithubUsername('');
@@ -60,32 +40,6 @@ export default function DevForm({ onSubmit }) {
           value={techs}
           onChange={e => setTechs(e.target.value)}
         />
-      </div>
-
-      <div className="input-group">
-        <div className="input-block">
-          <label htmlFor="latitude">Latitude</label>
-          <input
-            type="number"
-            id="latitude"
-            name="latitude"
-            required
-            value={latitude}
-            onChange={e => setLatitude(e.target.value)}
-          />
-        </div>
-
-        <div className="input-block">
-          <label htmlFor="longitude">Longitude</label>
-          <input
-            type="number"
-            id="longitude"
-            name="longitude"
-            required
-            value={longitude}
-            onChange={e => setLongitude(e.target.value)}
-          />
-        </div>
       </div>
 
       <button type="submit">Salvar</button>
